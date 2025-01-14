45a0f2d06b0db7d27d6f1ffc2c420ab9540c41ef
glebson1988 <glebson1988@yandex.ru>
2025-01-14 20:23:28 +0100

    Create a crowdfunding campaign

--- programs/sol-crowdfunding/src/lib.rs
+++ programs/sol-crowdfunding/src/lib.rs
@@ -6,11 +6,35 @@ declare_id!("7s9rxZqqDKkcn2QPvhBszuxEwiR7CPL4sLNdxiQpCKEJ");
 pub mod sol_crowdfunding {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign: &mut _ = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *ctx.accounts.user.key;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 9000,
+        seeds = [b"CAMPAIGN_DEMO".as_ref(), user.key().as_ref()],
+        bump
+    )]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct Campaign {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub amount_donated: u64
+}
